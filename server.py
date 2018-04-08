fbcd169a29c53a7abbc9e29a215277f34074e71f
scrappyzhang <a7478317@163.com>
2018-04-08 14:46:35 +0800

    服务器基本入口

---
+++ server.py
@@ -0,0 +1,48 @@
+"""服务器入口"""
+'''
+@Time    : 2018/4/8 下午2:23
+@Author  : scrappy_zhang
+@File    : server.py
+'''
+
+import tornado.web
+import tornado.ioloop
+import tornado.options
+import tornado.httpserver
+
+import os
+import redis
+
+import torndb
+import config
+from urls import urls
+
+from tornado.options import options, define
+from tornado.web import RequestHandler
+
+define("port", default=8000, type=int, help="run server on the given port")
+
+
+class Application(tornado.web.Application):
+    def __init__(self, *args, **kwargs):
+        super(Application, self).__init__(*args, **kwargs)
+        self.db = torndb.Connection(**config.mysql_options)
+        self.redis = redis.StrictRedis(**config.redis_options)
+
+
+def main():
+    options.log_file_prefix = config.log_path
+    options.logging = config.log_level
+    tornado.options.parse_command_line()
+
+    app = Application(
+        urls,
+        **config.settings
+    )
+    http_server = tornado.httpserver.HTTPServer(app)
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.current().start()
+
+
+if __name__ == '__main__':
+    main()
---
+++ urls.py
@@ -0,0 +1,12 @@
+"""URl路由"""
+'''
+@Time    : 2018/4/8 下午2:43
+@Author  : scrappy_zhang
+@File    : urls.py
+'''
+
+import os
+
+urls =[
+
+]
