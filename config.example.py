a0386712cccbdd0109d447401daa94b5e661a2ca
scrappyzhang <a7478317@163.com>
2018-04-08 14:27:34 +0800

    配置文件

---
+++ config.example.py
@@ -0,0 +1,22 @@
+"""配置文件"""
+'''
+@Time    : 2018/4/8 下午2:10
+@Author  : scrappy_zhang
+@File    : config.example.py
+'''
+
+import os
+
+# Application配置参数
+settings = dict(
+
+)
+
+# 数据库配置参数
+
+mysql_options = dict(
+    host="127.0.0.1",
+    database="ihome_tornado",
+    user="root",
+    password="password"
+)
--- config.py
+++ config.py
@@ -2,5 +2,34 @@
 '''
 @Time    : 2018/4/8 下午2:10
 @Author  : scrappy_zhang
-@File    : config.py
+@File    : config.example.py
 '''
+
+import os
+
+# Application配置参数
+settings = dict(
+
+)
+
+
+# 数据库配置参数
+mysql_options = dict(
+    host="127.0.0.1",
+    database="ihome_tornado",
+    user="root",
+    password="hitzzy"
+)
+
+# redis配置参数
+redis_options = dict(
+    host="127.0.0.1",
+    port=6379
+)
+
+# 日志配置
+log_path = os.path.join(os.path.dirname(__file__), "logs/log")
+log_level = "debug"
+
+# 密码加密密钥
+passwd_hash_key = "nlgCjaTXQX2jpupQFQLoQo5N4OkEmkeHsHD9+BBx2WQ="
